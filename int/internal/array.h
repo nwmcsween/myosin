07df9d7ae7a2ae79401e670998246f31015b7261
Nathan McSween <nwmcsween@gmail.com>
2012-09-25 02:35:28 +0000

    array.h - array related macros / functions

---
+++ int/internal/array.h
@@ -0,0 +1,12 @@
+/*@doc
+ * @desc: array related marcros and functions.
+ */
+#ifndef ARRAY_H
+#define ARRAY_H
+
+/*@doc
+ * @desc: get the length of an array
+ */
+#define arrlen(arr) (sizeof(arr) / sizeof(arr[0]))
+
+#endif /* ARRAY_H */
